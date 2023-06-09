395e840319309c8f9d36f64793d54476b6c983df
ldydek <91955435+ldydek@users.noreply.github.com>
2023-06-09 16:00:41 +0200

    Add files via upload

---
+++ Hibernate & JPA/Code/06/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final Session session = getSession();
+
+        try {
+            Transaction tx = session.beginTransaction();
+            Product table = session.get(Product.class, 10);
+            Invoice invoice1 = session.get(Invoice.class, 12);
+            table.sell(invoice1, 1);
+            tx.commit();
+        } finally {
+            session.close();
+        }
+    }
+}
