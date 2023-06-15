b152418622e2baad8b8992de1f805be6c91f8aaf
ldydek <91955435+ldydek@users.noreply.github.com>
2023-06-15 23:25:24 +0200

    Add files via upload

---
+++ Entity Framework/Kod/01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ŁukaszDydekEFProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj nazwę produktu");
+            string prodName = Console.ReadLine();
+
+            Console.WriteLine("Poniżej lista produktów zarejestrowanych w naszej bazie danych");
+            ProductContext productContext = new ProductContext();
+            Product product = new Product { ProductName = prodName };
+            productContext.Products.Add(product);
+            productContext.SaveChanges();
+
+            var query = from prod in productContext.Products
+                        select prod.ProductName;
+
+            foreach (var pName in query)
+            {
+                Console.WriteLine(pName);
+            }
+        }
+    }
+}
