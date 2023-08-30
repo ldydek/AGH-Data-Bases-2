cebafa362cafe027fdfe41d8fcef0272fef8116e
ldydek <91955435+ldydek@users.noreply.github.com>
2023-08-30 20:39:44 +0200

    Add files via upload

---
+++ Entity Framework/Kod/05/ProductContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ŁukaszDydekEFProducts
+{
+    internal class ProductContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Datasource=ProductsDatabase");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+    }
+}
